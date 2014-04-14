1ad5a616ea2edd7044f09c8359b075e9590b9913
Adrian Wądrzyk <adrian.wadrzyk@gmail.com>
2014-04-14 23:53:50 +0200

    Improve limitation of places where asteroid can be created

--- scripts/lib/asteroidGenerator.js
+++ scripts/lib/asteroidGenerator.js
@@ -6,21 +6,41 @@ define(["asteroid"], function (Asteroid) {
     var AsteroidGenerator = function (canvas) {
         this.canvas = canvas;
         this.asteroids = [];
+
+        this.quarterSize = {
+            x: 100,
+            y: 100
+        };
+
+        this.generateQuarters();
     };
 
     AsteroidGenerator.prototype.ASTEROID_LIMIT = 10;
-    AsteroidGenerator.prototype.quarters = [
-        {x: 0,   y: 0},
-        {x: 200, y: 0},
-        {x: 400, y: 0},
-        {x: 600, y: 0},
-        {x: 0,   y: 200},
-        {x: 600, y: 200},
-        {x: 0,   y: 400},
-        {x: 200, y: 400},
-        {x: 400, y: 400},
-        {x: 600, y: 400}
-    ];
+    AsteroidGenerator.prototype.TIME_INTERVAL  = 1000;
+
+    AsteroidGenerator.prototype.generateQuarters = function () {
+        var stepX = this.quarterSize.x,
+            stepY = this.quarterSize.y,
+            x = 0, y = 0;
+
+        this.quarters = [];
+
+        for ( ; x + stepX < this.canvas.width; x += stepX) {
+            this.quarters.push({x: x, y: y});
+        }
+
+        for ( ; y + stepY < this.canvas.height; y += stepY) {
+            this.quarters.push({x: x, y: y});
+        }
+
+        for ( ; x - stepX >= 0; x -= stepX) {
+            this.quarters.push({x: x, y: y});
+        }
+
+        for ( ; y - stepY >= 0; y -= stepY) {
+            this.quarters.push({x: x, y: y});
+        }
+    };
 
     AsteroidGenerator.prototype.generate = function () {
         setInterval(function () {
@@ -31,16 +51,16 @@ define(["asteroid"], function (Asteroid) {
 
                 this.asteroids.push(asteroid);
             }
-        }.bind(this), 1000);
+        }.bind(this), this.TIME_INTERVAL);
     };
 
     AsteroidGenerator.prototype.getRandomPosition = function () {
-        var quarterId = Math.floor((Math.random() * this.ASTEROID_LIMIT) + 1),
+        var quarterId = Math.floor((Math.random() * this.quarters.length)),
             quarter   = this.quarters[quarterId];
 
         return {
-            x: Math.floor((Math.random() * 200) + quarter.x),
-            y: Math.floor((Math.random() * 200) + quarter.y)
+            x: Math.floor((Math.random() * this.quarterSize.x) + quarter.x),
+            y: Math.floor((Math.random() * this.quarterSize.y) + quarter.y)
         };
     };
 
